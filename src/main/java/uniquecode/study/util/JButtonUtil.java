40528cab63a0b3bd317d2b5dfe0eaeb6bfbce16c
Patryk Pszeniczny <vcode1@onet.pl>
2025-06-08 16:06:31 +0200

    commit [JButtonUtil scall icon to 80% of height)

--- src/main/java/uniquecode/study/util/JButtonUtil.java
+++ src/main/java/uniquecode/study/util/JButtonUtil.java
@@ -11,7 +11,7 @@ public class JButtonUtil {
     }
     public static JButton registerButton(ButtonType buttonType, String display, String iconSource,
                                         Dimension dimension) {
-        Icon icon = getIcon(iconSource);
+        Icon icon = getIcon(iconSource, dimension.height);
         JButton button;
         if(buttonType==ButtonType.ICON){
             button=new JButton(icon);
@@ -30,12 +30,12 @@ public class JButtonUtil {
         return registerButton(ButtonType.DEFAULT, display, iconSource, dimension);
     }
     public static JButton registerButton(String display, String iconSource) {
-        return registerButton(ButtonType.ICON, display, iconSource, new Dimension(64, 64));
+        return registerButton(ButtonType.ICON, display, iconSource, new Dimension(48, 48));
     }
     public static JButton registerButton(ButtonType buttonType, String display, String iconSource, int width, int height) {
-        return registerButton(buttonType, display, iconSource, new Dimension(width, height));
+        return registerButton(ButtonType.DEFAULT, display, iconSource, new Dimension(width, height));
     }
-    public static Icon getIcon(String iconSource) {
+    public static Icon getIcon(String iconSource, int height) {
         if (!iconSource.matches("(?i)^.+\\.(jpg|jpeg|png|gif|bmp|webp|tiff)$")) {
             return UIManager.getIcon(iconSource);
         }
@@ -47,7 +47,7 @@ public class JButtonUtil {
         }
 
         ImageIcon icon = new ImageIcon(resource);
-        Image scaled = icon.getImage().getScaledInstance(32, 32, Image.SCALE_SMOOTH);
+        Image scaled = icon.getImage().getScaledInstance((int) (height*0.8), (int) (height*0.8), Image.SCALE_SMOOTH);
         return new ImageIcon(scaled);
     }
 
